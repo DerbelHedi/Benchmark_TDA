b53e913e21ece008e872e72a6d10cae80300666b
DerbelHedi <hediderbel68@gmail.com>
2023-07-18 11:25:36 +0200

    my third commit

--- build_edge.py
+++ build_edge.py
@@ -5,6 +5,7 @@ import gudhi as gd
 def complex_edge(points,max_edge_length=None,sparse=None,max_dimension=None,nb_iterations=None):  
         RC=gd.RipsComplex(points=points,max_edge_length=max_edge_length, sparse=sparse).create_simplex_tree(max_dimension=max_dimension)
         RC.collapse_edges(nb_iterations=nb_iterations)
+        RC.expansion(2)
         
         return RC        
 
